74b1b6648ba2b71195e61a1c9500eefc8092833b
Lauren Alexander <a.lauren@wustl.edu>
2024-05-10 16:17:22 -0500

    oopsie check

--- src/AttractionDetail.js
+++ src/AttractionDetail.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import './Attractions.css'; // Importing the CSS file
+import './AttractionDetail.css'; // Importing the CSS file
+import './Attractions.css'; 
 
 function AttractionDetail() {
   const { id } = useParams();  // `id` here is the place_id from Google Places
