4ff7b81261e68fec80178d84dc81e3c493c6b013
Lauren Alexander <a.lauren@wustl.edu>
2024-05-10 16:41:18 -0500

    attraction detail changes

--- src/AttractionDetail.js
+++ src/AttractionDetail.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import './AttractionDetail.css'; // Importing the CSS file
+import './attractionDetail.css'; // Importing the CSS file
 import './Attractions.css'; 
 
 function AttractionDetail() {
