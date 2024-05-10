3d4ddb041b99c8ead01cdf9d9b538d524bbcd4a8
Lauren Alexander <a.lauren@wustl.edu>
2024-05-10 16:01:23 -0500

    detail

--- src/AttractionDetail.js
+++ src/AttractionDetail.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import './Attractions.css'; // Importing the CSS file
 
 function AttractionDetail() {
   const { id } = useParams();  // `id` here is the place_id from Google Places
